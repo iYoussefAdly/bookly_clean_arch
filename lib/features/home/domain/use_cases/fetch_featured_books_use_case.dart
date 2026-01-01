dcbae0a3ae675f6a0304b0565bb375d333f2333a
youssefadly <youssefadly332@gmail.com>
2026-01-01 15:05:58 +0200

    refactor to use case class

--- lib/features/home/domain/use_cases/fetch_featured_books_use_case.dart
+++ lib/features/home/domain/use_cases/fetch_featured_books_use_case.dart
@@ -1,14 +1,19 @@
 import 'package:bookly_clean_arch/core/errors/failure.dart';
+import 'package:bookly_clean_arch/core/use_cases/use_case.dart';
 import 'package:bookly_clean_arch/features/home/domain/entities/book_entity.dart';
 import 'package:bookly_clean_arch/features/home/domain/repos/home_repo.dart';
 import 'package:dartz/dartz.dart';
 
-class FetchFeaturedBooksUseCase {
+class FetchFeaturedBooksUseCase extends UseCase<List<BookEntity>,NoParam> {
   final HomeRepo homeRepo;
 
   FetchFeaturedBooksUseCase({required this.homeRepo});
-  Future<Either<Failure, List<BookEntity>>> fetchFeaturedBooks() {
-    // يقدر يستخدم الفانكشن دي ولا لأ user  هل ال  permision  هنا بنشوف ال 
-    return homeRepo.fetchFeaturedBooks();
+  
+  @override
+  Future<Either<Failure, List<BookEntity>>> call([NoParam? param]) {
+    throw UnimplementedError();
   }
+  
 }
+
+class NoParam{}
