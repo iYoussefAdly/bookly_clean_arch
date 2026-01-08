c66dc8b43f07979c69f86e6dc60bc13ea393f259
youssefadly <youssefadly332@gmail.com>
2026-01-08 16:51:30 +0200

    register adapter and open featured box

--- lib/constants.dart
+++ lib/constants.dart
@@ -3,3 +3,4 @@ import 'package:flutter/cupertino.dart';
 const kPrimaryColor = Color(0xff0F0B1F);
 const kTransitionDuration = Duration(milliseconds: 250);
 const kGtSectraFine = "GT Sectra Fine Regular";
+const kFeaturedBooks = "featured_box";
--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,13 @@
 import 'package:bookly_clean_arch/constants.dart';
 import 'package:bookly_clean_arch/core/utils/app_router.dart';
+import 'package:bookly_clean_arch/features/home/domain/entities/book_entity.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:hive_flutter/adapters.dart';
 
-void main() {
+void main() async{
+  Hive.registerAdapter(BookEntityAdapter());
+  await Hive.openBox(kFeaturedBooks);
   runApp(const BooklyCleanArch());
 }
 
