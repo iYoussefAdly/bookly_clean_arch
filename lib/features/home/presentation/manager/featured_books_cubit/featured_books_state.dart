2082fa0cfd343863792dde283c10dd716bef96d1
youssefadly <youssefadly332@gmail.com>
2026-01-14 15:54:21 +0200

    fetch featured book cubit creation

---
+++ lib/features/home/presentation/manager/featured_books_cubit/featured_books_state.dart
@@ -0,0 +1,14 @@
+part of 'featured_books_cubit.dart';
+
+@immutable
+sealed class FeaturedBooksState {}
+final class FeaturedBooksInitial extends FeaturedBooksState {}
+final class FeaturedBooksLoading extends FeaturedBooksState {}
+final class FeaturedBooksSuccess extends FeaturedBooksState {
+  final List<BookEntity>books;
+  FeaturedBooksSuccess({required this.books});
+}
+final class FeaturedBooksFailure extends FeaturedBooksState {
+  final String errorMessage;
+  FeaturedBooksFailure({required this.errorMessage});
+}
