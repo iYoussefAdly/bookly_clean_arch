c7dd10664a2e02985b616e97604077eb734d6b4c
youssefadly <youssefadly332@gmail.com>
2026-01-15 17:45:28 +0200

    reach 70% of featured list view to make a new request

--- lib/features/home/presentation/views/widgets/feature_item_list_view.dart
+++ lib/features/home/presentation/views/widgets/feature_item_list_view.dart
@@ -1,22 +1,66 @@
 import 'package:bookly_clean_arch/features/home/domain/entities/book_entity.dart';
+import 'package:bookly_clean_arch/features/home/presentation/manager/featured_books_cubit/featured_books_cubit.dart';
 import 'package:bookly_clean_arch/features/home/presentation/views/widgets/custom_image.dart';
 import 'package:flutter/cupertino.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
-class FeatureItemListView extends StatelessWidget {
-  const FeatureItemListView({super.key, required this.books});
+class FeatureItemListView extends StatefulWidget {
+  const FeatureItemListView({
+    super.key,
+    required this.books,
+  });
+
   final List<BookEntity> books;
+
+  @override
+  State<FeatureItemListView> createState() => _FeatureItemListViewState();
+}
+
+class _FeatureItemListViewState extends State<FeatureItemListView> {
+  final ScrollController _scrollController = ScrollController();
+  bool _hasReached70Percent = false;
+
+  @override
+  void initState() {
+    super.initState();
+
+    _scrollController.addListener(() {
+      if (!_scrollController.hasClients) return;
+
+      final current = _scrollController.position.pixels;
+      final max = _scrollController.position.maxScrollExtent;
+
+      if (max <= 0) return;
+
+      final progress = current / max;
+
+      if (progress >= 0.7 && !_hasReached70Percent) {
+        _hasReached70Percent = true;
+
+        context.read<FeaturedBooksCubit>().fetchFeaturedBooks();
+      }
+    });
+  }
+  @override
+  void dispose() {
+    _scrollController.dispose();
+    super.dispose();
+  }
   @override
   Widget build(BuildContext context) {
     return SizedBox(
       height: MediaQuery.of(context).size.height * 0.24,
       child: ListView.builder(
-        physics: BouncingScrollPhysics(),
+        controller: _scrollController,
+        physics: const BouncingScrollPhysics(),
         scrollDirection: Axis.horizontal,
-        itemCount: books.length,
+        itemCount: widget.books.length,
         itemBuilder: (context, index) {
           return Padding(
             padding: const EdgeInsets.symmetric(horizontal: 10),
-            child: CustomImage(image: books[index].image??''),
+            child: CustomImage(
+              image: widget.books[index].image ?? '',
+            ),
           );
         },
       ),
